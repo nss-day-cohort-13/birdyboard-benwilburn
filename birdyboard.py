76ab3dc0fe225a40314c2db8373d068a105c08b5
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-08-07 21:54:00 -0500

    added secondary menu

--- birdyboard.py
+++ birdyboard.py
@@ -1,11 +1,19 @@
 import uuid
 import pickle
+import collections
 import serialization
 from chirp import *
 from user import *
 from conversation import *
 
 class Birdyboard:
+
+    def __init__(self):
+
+        self.users_directory = list()
+        self.public_chirps = list()
+        self.private_chirps = list()
+        self.current_user = None
 
     def initial_menu(self):
 
@@ -27,28 +35,59 @@ class Birdyboard:
 
             if selection == '1':
 
+                counter = 1
+
                 print('Enter Full Name')
                 fullname = input('fullname >  ')
                 print('Enter Username')
                 username = input('username >  ')
-
+                user_codex = User(fullname, username)
+                self.users_directory.append(user_codex)
+                print('users', self.users_directory)
+                serialization.serialize('users.txt', self.users_directory)
+                self.current_user = username
+                show_menu()
 
             if selection == '2':
 
                 select_user()
+                show_menu()
 
             if selection == '3':
 
-
-                create_public_chirps()
+                create_public_chirp()
 
             if selection == '4':
 
-                create_private_chirps()
+                create_private_chirp()
 
             if selection == '5':
 
                 view_all_chirps()
+
+    def show_menu(self):
+
+        print('1.) New Public Chirp')
+
+        print('2.) New Private Chirp')
+
+        print('3.) View Chirps')
+
+        print('4.) Exit')
+
+        selection = input("What do you want to do? >  ")
+
+        if selection == '1':
+
+            create_public_chirp()
+
+        if selection == '2':
+
+            create_private_chirp()
+
+        if selection == '3':
+
+            view_all_chirps()
 
 if __name__ == '__main__':
     birdyboard = Birdyboard()
--- serialization.py
+++ serialization.py
@@ -1,10 +1,9 @@
-def serialize(self, file, item):
+import pickle
+def serialize(file, item):
     with open(file, 'wb+') as f:
         pickle.dump(item, f)
-    with open(file, 'rb') as f:
-        items = f.read()
 
-def deserialize(self, file):
+def deserialize(file):
     try:
         with open(file, 'rb+') as f:
             items = pickle.load(f)
